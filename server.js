5a38d1eebbcdcb399157dec6e3d791e2a83b4bc2
karnam-Ajay <96416861+karnam-Ajay@users.noreply.github.com>
2024-11-07 10:35:33 +0000

    commentadded

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use('/api/contacts',require("./routes/contactRoutes"));  // her use() method acts as middleware
+app.use('/api/contacts',require("./routes/contactRoutes"));  // here use() method acts as middleware
 
 app.listen(port,()=>{
     console.log(`app is running on ${port}`);
