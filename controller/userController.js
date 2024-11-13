2e0f34a54054a5bc0925ce0f686094da805e6f68
karnam-Ajay <96416861+karnam-Ajay@users.noreply.github.com>
2024-11-13 11:17:48 +0000

    user schema model

--- controller/userController.js
+++ controller/userController.js
@@ -1,9 +1,25 @@
-const asyncHandler = require("express-async-handler")
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userSchema")
 // @desc user registration
 // @route Post api/users/register
 // @access public
 
 const registerUser = asyncHandler(async (req,res)=>{
+    const {username,email,password} = req.body;
+
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+
+    const userAvailable =  await User.findOne({email});
+
+    if (userAvailable) {
+        res.status(400);
+        throw new Error("User already registered");
+    }
+
+
     res.json({message:"register the user"});
 });
 
